2574aaa9f15cfbcf9666b41d0a4db1f805cc58fd
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-22 22:38:17 +0200

    sidebar-vue

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Home from "../views/Home.vue"
+import About from "../views/About.vue"
+
+import { createRouter, createWebHistory } from "vue-router"
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+  ],
+})
+
+export default router
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="about-page">
+    <h1>About</h1>
+    <p>This is the about page</p>
+
+    <p>To zadanie to jest test polskiej czcionki.</p>
+    <p>Jak na razie piszę bez poslich znaków.</p>
+    <p>ładana ta łamigłówka</p>
+  </main>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style></style>
